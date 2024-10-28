06afb2acc1da48beed84d946ba42a778d55936fc
ocluf <fulco.taen@gmail.com>
2024-10-28 15:10:09 +0100

    x

--- src/routes/stripe/stripe.ts
+++ src/routes/stripe/stripe.ts
@@ -1,6 +1,7 @@
 import Stripe from 'stripe';
 import { STRIPE_SECRET_KEY } from '$env/static/private';
 
+//TODO: Update to latest stripe client but seems to cause error
 export const stripeClient = new Stripe(STRIPE_SECRET_KEY, {
 	apiVersion: '2024-04-10',
 	httpClient: Stripe.createFetchHttpClient()
