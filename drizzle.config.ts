d12c0385e28a53eba336a63895e6313973b873bd
Fulco <fulco.taen@gmail.com>
2024-11-13 12:52:13 +0100

    Update drizzle.config.ts

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,7 +2,7 @@ import type { Config } from 'drizzle-kit';
 export default {
 	schema: './src/lib/server/database/schema.ts',
 	out: './drizzle',
-	dialect: 'sqlite',
+	dialect: 'turso',
 	dbCredentials: {
 		url: 'file:local.db'
 	}
