b6f3d5c87a105929e643495ad4eef22128a26135
ocluf <fulco.taen@gmail.com>
2024-11-16 11:57:57 +0100

    refactor: wrap user data display in Container component

--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -1,7 +1,13 @@
 <script lang="ts">
+	import Container from '$lib/components/Container.svelte';
+
 	import type { PageData } from './$types';
 
 	let { data }: { data: PageData } = $props();
 </script>
 
-<pre>{JSON.stringify(data.user, null, 2)}</pre>
+<Container>
+	<div class="my-10">
+		<pre>{JSON.stringify(data.user, null, 2)}</pre>
+	</div>
+</Container>
